246e8f2ae45b86ec93cde387480ada56dd0ebb8f
Gio <giomascaro@yahoo.com>
2024-12-17 23:57:20 +0100

    fa

--- serveur.py
+++ serveur.py
@@ -1,20 +1,30 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_cors import CORS  # Importer CORS
 import sqlite3
 from modele import Product
 
 app = Flask(__name__)
 
-# Route pour récupérer tous les produits
+CORS(app)
+
 @app.route("/products", methods=["GET"])
 def get_products():
-    conn = sqlite3.connect("db.sql")
+    start = request.args.get("id_start", default=0, type=int)  # Default à 0 si non précisé
+    limit = 36  
+
+    conn = sqlite3.connect("./db.sql")
     cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM products")
+    cursor.execute("""
+        SELECT * FROM products
+        WHERE id >= ?
+        LIMIT ?;
+    """, (start, limit))
+    
     rows = cursor.fetchall()
     conn.close()
 
-    # Convertir les résultats en objets Product
+    # Transformer les résultats en objets Product
     products = [
         Product(id=row[0], name=row[1], description=row[2], price=row[3], image_url=row[4]).to_dict()
         for row in rows
